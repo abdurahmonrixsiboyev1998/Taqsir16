8bffd26956f0879520871df512aa7049e36d390f
abdurahmonrixsiboyev1998 <abduraxmonabdullayev87@gmail.com>
2024-05-07 13:45:26 +0500

    Taqsir16

--- main.go
+++ main.go
@@ -17,71 +17,52 @@ const (
 )
 
 func main() {
-	postgresqlDbInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+	postgresqlDbInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
-
-	db, err := sql.Open("postgres", postgresqlDbInfo)
+	num, err := sql.Open("postgres", postgresqlDbInfo)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
+	defer num.Close()
 
-	err = db.Ping()
+	err = num.Ping()
 	if err != nil {
 		log.Fatal("failed to ping", err)
 	}
 
 	fmt.Println("successfully connected")
+	send(num, 1, 2)
+	// accept(num,4,5)
+	// block(num,3,5)
+	// unblock(num,3,5)
 
-	err = sendFriendRequest(db, 4, 5)
-	if err != nil {
-		log.Println("Error sending friend request:", err)
-	}
+}
 
-	err = acceptFriendRequest(db, 4, 5)
+func send(num *sql.DB, jonatuvchiID int, qabul_qiluvchiID int) {
+	_, err := num.Exec("INSERT INTO friendships (user_id, friend_id, status) VALUES ($1, $2, $3)", jonatuvchiID, qabul_qiluvchiID, "requested")
 	if err != nil {
-		log.Println("Error accepting friend request:", err)
-	}
-
-	err = blockUser(db, 3, 5)
-	if err != nil {
-		log.Println("Error blocking user:", err)
-	}
-
-	err = unblockUser(db, 3, 5)
-	if err != nil {
-		log.Println("Error unblocking user:", err)
+		fmt.Println("Error sending friend request:", err)
 	}
 }
 
-func sendFriendRequest(db *sql.DB, senderID, receiverID int) error {
-	_, err := db.Exec("INSERT INTO friendships (user_id, friend_id, status) VALUES ($1, $2, $3)", senderID, receiverID, "requested")
+func Accept(num *sql.DB, jonatuvchiID int, qabul_qiluvchiID int) {
+	_, err := num.Exec("UPDATE friendships SET status = $1 WHERE user_id = $2 AND friend_id = $3", "accepted", qabul_qiluvchiID, jonatuvchiID)
 	if err != nil {
-		return err
+		fmt.Println("Error accepting friend request:", err)
 	}
-	return nil
 }
 
-func acceptFriendRequest(db *sql.DB, senderID, receiverID int) error {
-	_, err := db.Exec("UPDATE friendships SET status = $1 WHERE user_id = $2 AND friend_id = $3", "accepted", receiverID, senderID)
+func Block(num *sql.DB, userID int, blockedUserID int) {
+	_, err := num.Exec("UPDATE friendships SET status = $1 WHERE user_id = $2 AND friend_id = $3", "blocked", userID, blockedUserID)
 	if err != nil {
-		return err
+		fmt.Println("Error blocking user:", err)
 	}
-	return nil
 }
 
-func blockUser(db *sql.DB, userID, blockedUserID int) error {
-	_, err := db.Exec("UPDATE friendships SET status = $1 WHERE user_id = $2 AND friend_id = $3", "blocked", userID, blockedUserID)
+func Unblock(num *sql.DB, userID int, unblockedUserID int) {
+	_, err := num.Exec("UPDATE friendships SET status = $1 WHERE user_id = $2 AND friend_id = $3", "requested", userID, unblockedUserID)
 	if err != nil {
-		return err
+		fmt.Println("Error unblocking user:", err)
 	}
-	return nil
-}
-
-func unblockUser(db *sql.DB, userID, unblockedUserID int) error {
-	_, err := db.Exec("UPDATE friendships SET status = $1 WHERE user_id = $2 AND friend_id = $3", "requested", userID, unblockedUserID)
-	if err != nil {
-		return err
-	}
-	return nil
 }
